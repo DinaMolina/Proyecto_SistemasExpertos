8d8b59ca9c52e7e1e1ada34f144589f8ae4ea35c
DinaMolina <48228193+DinaMolina@users.noreply.github.com>
2020-11-19 00:10:34 -0600

    commit

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistroClienteComponent } from './paginas/registro-cliente/registro-cliente.component';
+import { RegistroEmpresaComponent } from './paginas/registro-empresa/registro-empresa.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DesignComponent } from './paginas/design/design.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [ 
+  {
+    path: '', component: LandingPageComponent
+  },
+  {
+    path: 'registro-cliente', component: RegistroClienteComponent
+  }, 
+  {
+    path: 'registro-empresa', component: RegistroEmpresaComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'diseño', component: DesignComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } 
+
+
--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { RegistroClienteComponent } from './paginas/registro-cliente/registro-cliente.component';
+import { RegistroEmpresaComponent } from './paginas/registro-empresa/registro-empresa.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DesignComponent } from './paginas/design/design.component';
+import { QuillModule } from 'ngx-quill';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroClienteComponent,
+    RegistroEmpresaComponent,
+    LoginComponent,
+    DesignComponent,
+    HeaderComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QuillModule.forRoot(), 
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
